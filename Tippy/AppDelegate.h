8dc9d5a693272d7894782c94be4c4aa64e13086c
Sanjana Meduri <sanjanameduri@fb.com>
2021-06-22 14:20:38 -0400

    Initial Commit

---
+++ Tippy/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Tippy
+//
+//  Created by Sanjana Meduri on 6/22/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Tippy/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Tippy
+//
+//  Created by Sanjana Meduri on 6/22/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
